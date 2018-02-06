5f5064402ae8477aced7c15b7cb2214acea8070d
Wildan Bimantoro <wildan.bimantoro@aitindo.com>
2018-02-06 19:01:09 +0700

    complete state 2, shooting mechanism

--- state2.js
+++ state2.js
@@ -1,4 +1,5 @@
-var barrel, velocity = 1000, nextFire = 0, fireRate = 200;
+var barrel, velocity = 1000, nextFire = 0, fireRate = 200, enemy,bullet,
+enemyGroup;
 
 demo.state2 = function () { };
 demo.state2.prototype = {
@@ -6,6 +7,7 @@ demo.state2.prototype = {
         game.load.image('base', 'assets/sprites/base.png');
         game.load.image('barrel', 'assets/sprites/barrel.png');
         game.load.image('bullet', 'assets/sprites/bullet.png');
+        game.load.image('barbarian','assets/sprites/barbarian-big.png');
     },
     create: function () {
         game.stage.backgroundColor = "#0099ff";
@@ -31,22 +33,48 @@ demo.state2.prototype = {
         barrel.anchor.setTo(0.1, 0.43);
         barrel.scale.setTo(5);
 
+        enemy = game.add.sprite(100,200,'barbarian');
+        game.physics.enable(enemy);
+
+        enemyGroup = game.add.group();
+        enemyGroup.enableBody = true;
+        enemyGroup.physicsBodyType = Phaser.Physics.ARCADE;
+
+        for (var i = 0; i < 3; i++){
+            enemyGroup.create(1300,350 * i + 100, 'barbarian');
+        }
+        enemyGroup.setAll('anchor.y',0.5);
+        enemyGroup.setAll('anchor.x',0.5);
+        // enemyGroup.setAll('scale.y',0.7);
+        enemyGroup.setAll('scale.x',0.7);
     },
     update: function () {
         barrel.rotation = game.physics.arcade.angleToPointer(barrel);
         if (game.input.activePointer.isDown) {
             this.fire();
         }
+
+        game.physics.arcade.overlap(bullets,enemy,this.hitEnemy);
+        game.physics.arcade.overlap(bullets,enemyGroup, this.hitGroup);
     },
     fire: function () {
         if(game.time.now > nextFire){
             nextFire = game.time.now + fireRate;
             console.log('firing');
-            var bullet = bullets.getFirstDead();
+            bullet = bullets.getFirstDead();
             bullet.reset(barrel.x, barrel.y);
 
             game.physics.arcade.moveToPointer(bullet, velocity);
             bullet.rotation = game.physics.arcade.angleToPointer(bullet);
         }
+    },
+    hitEnemy: function(){
+        console.log('hit the enemy');
+        enemy.kill();
+        bullet.kill();
+    },
+    hitGroup: function(b,e){
+        b.kill();
+        e.kill();
     }
 };
