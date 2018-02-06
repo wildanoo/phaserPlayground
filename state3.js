452e17705b2af70b26df70b06863dc80deb936b4
wildan <wildan.bimantoro@aitindo.com>
2018-02-06 21:55:38 +0700

    add audio state3

--- state3.js
+++ state3.js
@@ -1,9 +1,46 @@
 demo.state3 = function() {};
 demo.state3.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('button1','assets/sprites/button-1.png');
+        game.load.image('button2','assets/sprites/button-2.png');
+        game.load.image('button3','assets/sprites/button-3.png');
+        game.load.audio('pops','assets/sounds/buttonPops.mp3');
+    },
     create: function(){
         game.stage.backgroundColor = "#00cc99";
         addChangeEventListener();
+
+        sound = game.add.audio('pops');
+        sound.addMarker('low', 1.5 , 1);
+        sound.addMarker('high', 4.5 , 1);
+
+        var b1 = game.add.button(100,100, 'button1', function(){
+            changeState(null,1);
+        });
+        var b2 = game.add.button(200,200, 'button2', function(){
+            changeState(null,2);
+        });
+        var b3 = game.add.button(300,300, 'button3', function(){
+            // changeState(null,3);
+        });
+
+        b1.onInputDown.add(this.tint, b1);
+        b2.onInputDown.add(this.tint, b2);
+        b3.onInputDown.add(this.tint, b3);
+
+        b1.onInputUp.add(this.untint, b1);
+        b2.onInputUp.add(this.untint, b2);
+        b3.onInputUp.add(this.untint, b3);
+
     },
-    update: function(){}
+    tint: function(){
+        this.tint = 0xbbbbbb;
+        sound.stop();
+        sound.play('low');
+    },
+    untint: function(){
+        this.tint = 0xffffff;
+        sound.stop();
+        sound.play('high');
+    }
 };
